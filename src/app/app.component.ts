dc928f3b91a04231b30800f4fcbf2b71c1d3fd26
LeoBurnettMNL <catherine.tigas@gmail.com>
2018-07-11 02:31:07 +0800

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,35 +12,43 @@ export class AppComponent {
 	post: any;
 	description: string = "";
 	name: string = "";
+	primaryEmail:string ="";
 	titleAlert: string = "This field is required";
 	descriptionAlert: string = "This field is required";
+	emailAlert:string = "This field is required";
 	constructor(private fb: FormBuilder){
 
 		// specify the validation
 		this.rForm = fb.group({
 			'name' : [null, Validators.required],
 			'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+			'primaryEmail' : [null,Validators.email],
 			'validate' : ''
 		});
 	}
 	addPost(post){
 		this.description = post.description;
 		this.name = post.name;
+		this.primaryEmail = post.primaryEmail;
 	}
 	ngOnInit(){
 		this.rForm.get('validate').valueChanges.subscribe(
 			(validate) => {
 				if(validate == '1'){
 					this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
-					this.titleAlert = "You need to specify at least 3 characters";
+					this.rForm.get('primaryEmail').setValidators([Validators.required, Validators.email]);
 					this.rForm.get('description').setValidators([Validators.required, Validators.minLength(3)]);
+					this.titleAlert = "You need to specify at least 3 characters";
+					this.emailAlert = "Primary Email not valid.";
 					this.descriptionAlert = "You must specify a description between 30 to 500 characters";
 				}else{
 					this.rForm.get('name').setValidators([Validators.required);				
 					this.rForm.get('description').setValidators([Validators.required);				
+					this.rForm.get('primaryEmail').setValidators([Validators.required);				
 				}
 			this.rForm.get('name').updateValueAndValidity();
 			this.rForm.get('description').updateValueAndValidity();
+			this.rForm.get('primaryEmail').updateValueAndValidity();
 
 			}
 		)
